8dedf4783bda4dabc7ef506c89516220e1cfb6e2
kross <kirillchaikovsky8@gmail.com>
2026-05-07 22:30:50 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-    console.log(process.env.JWT_SECRET);
+    console.log(process.env.CLIENT_URL);
     const app = await NestFactory.create(AppModule);
     app.enableCors({
         origin: process.env.CLIENT_URL,
